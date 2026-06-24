d6372225f28a3fa7cad3055fe7a1ac9b0f2e3063
matias-cisnero <matiascisnero2014@gmail.com>
2026-06-24 01:48:20 -0300

    feat: añadir api con FastAPI para inferencia del modelo

---
+++ app.py
@@ -0,0 +1,38 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+import mlflow
+import pandas as pd
+from pydantic import BaseModel
+
+from config import MODEL_URI
+
+models = {}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    mlflow.set_tracking_uri("http://localhost:5000")
+
+    model = mlflow.pyfunc.load_model(MODEL_URI)
+    models["insurance_model"] = model
+    yield
+
+    models.clear()
+
+app = FastAPI(lifespan=lifespan)
+
+class InputData(BaseModel):
+    age: int
+    sex: str
+    bmi: float
+    children: int
+    smoker: str
+    region: str
+
+@app.post("/predict")
+def predict(data: InputData):
+    data_df = pd.DataFrame([data.model_dump()])
+
+    model = models["insurance_model"]
+    predictions = model.predict(data_df)
+
+    return {"predictions": predictions.tolist()}
